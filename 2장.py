7c9acf0cb2742c550cbe3d29b866dc7fc7f68f10
moove <mooveject@gmail.com>
2026-06-18 14:19:15 +0900

    Add files via upload

---
+++ 2장.py
@@ -0,0 +1,48 @@
+#2장
+print ("성공")
+print (5+3)
+print (2*8)
+print (6/3)
+print (3*(3+1))
+print ("풍선")
+print ("나비")
+print ("abcdefg")
+print ("10")
+print ("파이썬"*3)
+print (5>10)
+print (5<10)
+print (True)
+print (False)
+print (not True)
+print (not False)
+print (not (5>10))
+
+name = "해피"
+animal = "고양이"
+age = 4
+hobby= "낮잠"
+is_male = True
+print ("반려동물을 소개해 주세요.")
+print ("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "예요.")
+print (name + "는 " + str (age) +"살이고, " + hobby + "를 아주 좋아해요")
+#Str (자료값을 문자열로 바꿈)
+print (name, "는 ", age, "살이고, ", hobby, "를 아주 좋아해요")
+#+ 대신 , (쉼표) 사용 시, str 변형 안해도 숫자 출력 가능
+# 쉼표가 들어가면 4 살 <- 빈칸이 나옴
+
+"""
+print (int(3.5)) #Int (정수) - 소수점 X
+print (float(3)) #Float (실수형)
+print (3, "입니다.")
+print (type (3))
+#Type () 데이터의 형태 파악 + 형변환 확인
+# commend  /  -> 주석 처리 단축키
+#여러 줄 주석 처리 시 양끝에 
+# """
+
+#2-6장 실무 문제 풀이
+station = "사당"
+print (station+"행 열차가 들어오고 있습니다.")
+status = "상품준비"
+print ("주문상태 :", status)
+
